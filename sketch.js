98118222399fba84211ffaef6e3dcc823f37c771
FelipinReiDelasxsx <fa8376918@gmail.com>
2024-08-29 15:58:57 -0300

    add arquivo js css

---
+++ sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }
+  function setup() {
+  
+    createCanvas(400, 400);
+  
+    background("white")
+  
+  }
+  
+  function draw() {
+  
+    stroke("#000000");
+  
+    fill("#4CAF50");
+  
+    
+  
+    
+  
+    if (mouseIsPressed) {
+  
+      rect(mouseX, mouseY, 20, 35);
+  
+    }
+  
+  }
+  
+  
